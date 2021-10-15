0f4394bd1694e5d3685ae95a03d3ed8101eb00f9
Julio Alberto Fernandez Guerrero <jafernandezg9@mail.com>
2021-10-15 15:18:41 +0200

    Initial commit

---
+++ src/test/java/com/jafernandez/productsapp/ProductsAppTests.java
@@ -0,0 +1,16 @@
+package com.jafernandez.productsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.Assert.assertTrue;
+
+@SpringBootTest
+public class ProductsAppTests {
+
+    @Test
+    public void fakeTest() {
+        assertTrue(true);
+    }
+
+}
